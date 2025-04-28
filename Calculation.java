640a77949c413f65d7d68f9d911f25e327875553
zaynabali-12345 <zaynabali00786@gmail.com>
2025-04-28 14:09:48 +0530

    message

---
+++ Calculation.java
@@ -0,0 +1,10 @@
+public class Calculation {
+
+    public static void main(String[] args) {
+        int a=5,b=10;
+        System.out.println("Addition:"+(a+b));
+        System.out.println("Substraction:"+(a-b));
+        System.out.println("Multiplication:"+(a*b));
+        System.out.println("Division:"+(a/b));
+    }
+}
